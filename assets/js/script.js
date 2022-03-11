38f587a16d9d13a01fbf94b7ed162e465409562e
isabinvignat <ibinvignat@gmail.com>
2022-03-11 09:42:25 -0300

    trabajando en seccion somos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function)(){
-    $("a").click(function){
-        var gat =this.hash
+    $("a").click(function) {
+        var gat = this.hash
         $("html, body").animate({
             scrollTop: $(gat).offset().top-70
         }),
        800
-    })
-}
+    };
+};
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
