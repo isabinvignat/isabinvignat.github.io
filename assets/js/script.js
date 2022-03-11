3e131631c490c298e0c84ad5ff3bbb27ad7f1264
isabinvignat <ibinvignat@gmail.com>
2022-03-11 11:49:15 -0300

    smooth scroll con css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-$(document).ready(function)(){
-    $("a").click(function) {
-        var gat = this.hash
-        $("html, body").animate({
-            scrollTop: $(gat).offset().top-70
-        }),
-       800
-    };
-};
-
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
