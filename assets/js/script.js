6fac8f4ee2227956dff356c79c10b6171add8b12
isabinvignat <ibinvignat@gmail.com>
2022-03-12 19:00:43 -0300

    entrega final prueba

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+$(function () {
+  $("a").click(function (event) {
+      if (this.hash !== "") {
+          event.preventDefault();
+          var gato = this.hash;
+          $("html, body").animate({
+              scrollTop: $(gato).offset().top
+          }, 800);
+      }
+  });
+});
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
