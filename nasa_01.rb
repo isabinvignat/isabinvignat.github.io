7f9777e5b0bcaab8bed0d5dd5d66155b5121aff8
isabinvignat <ibinvignat@gmail.com>
2022-03-25 12:33:23 -0300

    Token

--- nasa_01.rb
+++ nasa_01.rb
@@ -2,21 +2,22 @@ require "uri"
 require "net/http"
 require "json"
 
-def pedido (dato)
+def request(url)
 url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=RjJBl8An5C5c2nhqyk6108kIc8O9LOv7WvTOpHRA")
 
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true
 
 request = Net::HTTP::Get.new(url)
-request [Postman--Token: "6qn885aIlWhuXqPx0cd539hAWBwcXRtsnwSiFD8"]
+request["Postman-Token"] ='1ddc4130-91f0-49b2-b459-fa40423e572a'
+
 response = https.request(request)
 JSON.parse(response.read_body)
 end
-dato = pedido("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=RjJBl8An5C5c2nhqyk6108kIc8O9LOv7WvTOpHRA")
+dato = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=RjJBl8An5C5c2nhqyk6108kIc8O9LOv7WvTOpHRA")
 
 def construir_pag_web(nasa)
-    datos= nasa.select{|llave| llave== "sol-10"}.values
+    datos= nasa['photos'].map{|llave| llave=='sol'}
     puts datos
      universo ="<!DOCTYPE html>\n<html lang='es-cl'>\n\t<head>\n\t</head>\n\t<body>\n\t\t\t<ul>"
     datos.each do |foto|
@@ -24,3 +25,6 @@ def construir_pag_web(nasa)
     end
      universo +="\n\t\t</ul>\n\t</body>\n\</html>"
      File.write('nasaweb.html',universo)
+    
+end
+construir_pag_web(dato)
