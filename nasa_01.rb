f0dafb5fa80bd7a0ead7f81c59bdf40621e31305
isabinvignat <ibinvignat@gmail.com>
2022-03-27 10:41:35 -0300

    Entrega prueba modulo

--- nasa_01.rb
+++ nasa_01.rb
@@ -17,11 +17,11 @@ end
 dato = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=RjJBl8An5C5c2nhqyk6108kIc8O9LOv7WvTOpHRA")
 
 def construir_pag_web(nasa)
-    datos= nasa['photos'].map{|llave| llave=='sol'}
+    datos= nasa["photos"].map{|llave| llave['img_src']}
     puts datos
      universo ="<!DOCTYPE html>\n<html lang='es-cl'>\n\t<head>\n\t</head>\n\t<body>\n\t\t\t<ul>"
-    datos.each do |foto|
-     universo += "\n\t\t\t<li><img src='#{foto}'>\n\t\t\t</li>"
+    datos.each do |image|
+     universo += "\n\t\t\t<li><img src='#{image}'>\n\t\t\t</li>"
     end
      universo +="\n\t\t</ul>\n\t</body>\n\</html>"
      File.write('nasaweb.html',universo)
